d21211293a2544a4bd7462a3f7e8d787e70e6ea9
lydiacho <sheecho0804@ewhain.net>
2023-11-24 16:44:47 +0900

    setting: Routing

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,12 +1,14 @@
 import { ThemeProvider } from "styled-components";
 import GlobalStyle from "./styles/GlobalStyle";
 import theme from "./styles/theme";
+import Router from "./Router";
 
 function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
+        <Router />
       </ThemeProvider>
     </>
   );
