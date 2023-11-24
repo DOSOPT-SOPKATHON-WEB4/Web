aa3bfd5fbe1280fb9fa4a14f7bc29d99f5d366b3
lydiacho <sheecho0804@ewhain.net>
2023-11-24 16:40:30 +0900

    setting: theme

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,9 +1,13 @@
+import { ThemeProvider } from "styled-components";
 import GlobalStyle from "./styles/GlobalStyle";
+import theme from "./styles/theme";
 
 function App() {
   return (
     <>
-      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+      </ThemeProvider>
     </>
   );
 }
---
+++ FrontEnd/src/styles/theme.js
@@ -0,0 +1,7 @@
+const colors = {};
+const fonts = {};
+const theme = {
+  colors,
+  fonts,
+};
+export default theme;
