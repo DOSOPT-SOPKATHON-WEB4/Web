90b43ececdbd0c23a10dbc28fa35599f6f7dc82d
Jun-min2 <cjm990622@naver.com>
2023-11-26 05:48:22 +0900

    feat: AddTag

--- FrontEnd/src/pages/AddTagPage.jsx
+++ FrontEnd/src/pages/AddTagPage.jsx
@@ -2,53 +2,48 @@ import styled from 'styled-components';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import { IcArrow } from '../assets/icons/0_icons';
+import { IcBack } from '../assets';
 
 const settings = {
   dots: true,
   infinite: true,
   speed: 500,
-  autoplay: true,
-  slidersToShow: 1,
-  slidersToScroll: 1,
-  centerMode: true,
-  centerPadding: '0px',
-  draggable: true,
+  slidersToShow: 2,
+  // slidersToScroll: 1,
+  // centerMode: true,
+  // centerPadding: '0px',
+  // draggable: true,
 };
 
 const AddTagPage = () => {
   return (
     <>
-      <IcArrow />
-      <St.AddTag>태그 추가</St.AddTag>
-      <St.TagContainer>
-        태그를 입력해주세요
-        <St.TagContainerInput placeholder='태그 이름 입력' />
-      </St.TagContainer>
-      <St.CarouselContainer>
-        <St.CarouselBox {...settings}>
-          <h2> Single Item</h2>
+      <St.AddTag>
+        <St.BackBox>
+          <IcBack />
+        </St.BackBox>
+        <St.AddTagContainer>케이크 추가하기</St.AddTagContainer>
+        <St.AddTagWrapper>케이크 이름을 입력해주세요</St.AddTagWrapper>
+        <St.AddTagLayout>
+          <St.TagContainerInput placeholder='뽀삐케이크' />
+          <St.AddTagBox></St.AddTagBox>
+        </St.AddTagLayout>
+        <St.SelectTheme>케이크 테마를 선택해주세요</St.SelectTheme>
 
+        <St.CarouselBox {...settings}>
           <div>
-            <h3>1</h3>
+            <St.CarouselImg src='src/assets/image/cake_new1_blue.png'></St.CarouselImg>
           </div>
           <div>
-            <h3>2</h3>
+            <St.CarouselImg src='src/assets/image/cake_new1_red.png'></St.CarouselImg>
           </div>
           <div>
-            <h3>3</h3>
-          </div>
-          <div>
-            <h3>4</h3>
-          </div>
-          <div>
-            <h3>5</h3>
-          </div>
-          <div>
-            <h3>6</h3>
+            <St.CarouselImg src='src/assets/image/cake_new1_green.png'></St.CarouselImg>
           </div>
         </St.CarouselBox>
-      </St.CarouselContainer>
+
+        <St.Button type='button'>다음</St.Button>
+      </St.AddTag>
     </>
   );
 };
@@ -56,25 +51,78 @@ const AddTagPage = () => {
 export default AddTagPage;
 
 const St = {
-  AddTag: styled.header``,
-  TagContainer: styled.section``,
+  AddTag: styled.main``,
+  BackBox: styled.div`
+    margin-top: 1.3rem;
+    margin-left: 1.2rem;
+  `,
+  AddTagContainer: styled.header`
+    margin-top: 2.5rem;
+    margin-left: 2rem;
+    ${({ theme }) => theme.fonts.s2};
+    color: ${({ theme }) => theme.colors.gray1};
+  `,
+  AddTagWrapper: styled.p`
+    margin-top: 0.8rem;
+    margin-left: 2.2rem;
+    ${({ theme }) => theme.fonts.b2};
+    color: ${({ theme }) => theme.colors.gray3};
+  `,
+
+  AddTagLayout: styled.div`
+    width: 17rem;
+    height: 3.1rem;
+    margin-left: 10.3rem;
+    margin-top: 4rem;
+  `,
+
+  AddTagBox: styled.div`
+    margin-top: 0.5rem;
+    height: 0.2rem;
+    width: 17rem;
+    background-color: ${({ theme }) => theme.colors.gray3};
+    border-radius: 0.9rem;
+  `,
   TagContainerInput: styled.input`
-    &::placeholder {
+    width: 17rem;
+    ${({ theme }) => theme.fonts.s3};
+    text-align: center;
+    color: ${({ theme }) => theme.colors.gray3};
+    border: none;
+    &::placeholder= {
     }
   `,
+  SelectTheme: styled.p`
+    ${({ theme }) => theme.fonts.s2};
+    color: ${({ theme }) => theme.colors.gray1};
+    margin-top: 6rem;
+    margin-left: 2rem;
+  `,
   CarouselBox: styled(Slider)`
-    width: 100%;
-    height: 100%;
     .slick-list {
       margin: 0 auto;
-      overflow-x: hidden;
     }
     .slick-dots {
       //슬라이드의 위치
       bottom: -10px;
     }
   `,
-  CarouselContainer: styled.div`
-    width: 60rem;
+  CarouselImg: styled.img`
+    margin-left: 7.3rem;
+    width: 22.9rem;
+    height: 20rem;
+  `,
+  CarouselContainer: styled.div``,
+
+  Button: styled.button`
+    margin-left: 2rem;
+    margin-top: 3.7rem;
+    padding: 2rem 11.7rem;
+    width: 33.5rem;
+    height: 5.6rem;
+    text-align: center;
+    border: none;
+    background-color: ${({ theme }) => theme.colors.white};
+    border-radius: 10rem;
   `,
 };
