a3354d4fa8d4021a726a96bf50a63665f7f064df
lydiacho <sheecho0804@ewhain.net>
2023-11-26 05:40:18 +0900

    style: PickCandle 캔들 이미지 모두 추출

--- FrontEnd/src/components/AddCandle/PickCandle.jsx
+++ FrontEnd/src/components/AddCandle/PickCandle.jsx
@@ -1,11 +1,39 @@
 import styled from 'styled-components';
+import { IcCandle1, IcCandle2, IcCandle3, IcCandle4, IcCandle5 } from '../../assets';
+import { useState } from 'react';
 
 const PickCandle = () => {
-  return <St.Wrapper>PickCandle</St.Wrapper>;
+  const CANDLE = [
+    <IcCandle1 key={1} />,
+    <IcCandle2 key={2} />,
+    <IcCandle3 key={3} />,
+    <IcCandle4 key={4} />,
+    <IcCandle5 key={5} />,
+  ];
+
+  const [selectedCandle, setSelectedCandle] = useState('');
+  return (
+    <St.Wrapper>
+      <IcCandle1 />
+      <IcCandle2 />
+      <IcCandle3 />
+      <IcCandle4 />
+      <IcCandle5 />
+    </St.Wrapper>
+  );
 };
 
 export default PickCandle;
 
 const St = {
-  Wrapper: styled.section``,
+  Wrapper: styled.section`
+    display: flex;
+    gap: 3.7rem;
+    justify-content: center;
+    margin: 0rem 5rem;
+
+    & > svg {
+      cursor: pointer;
+    }
+  `,
 };
