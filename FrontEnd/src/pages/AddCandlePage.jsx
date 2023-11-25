4610287f3326dcf4482dd66116291c2a5a427100
lydiacho <sheecho0804@ewhain.net>
2023-11-26 02:00:57 +0900

    style: progressbar 배치

--- FrontEnd/src/pages/AddCandlePage.jsx
+++ FrontEnd/src/pages/AddCandlePage.jsx
@@ -1,22 +1,47 @@
 import styled from 'styled-components';
-import { IcBack } from '../assets';
+import { IcBack, IcProgressGrey, IcProgressRed } from '../assets';
 
 const AddCandlePage = () => {
+  // 스텝에 따라서 IcProgress 조건부 렌더링 시키기
   return (
-    <div>
+    <St.Wrapper>
       <St.Header>
         <IcBack />
+        <St.ProgressBar>
+          <IcProgressRed />
+          <IcProgressGrey />
+          <IcProgressGrey />
+          <IcProgressGrey />
+          <IcProgressGrey />
+        </St.ProgressBar>
       </St.Header>
-    </div>
+    </St.Wrapper>
   );
 };
 
 export default AddCandlePage;
 
 const St = {
+  Wrapper: styled.section``,
   Header: styled.header`
     border: 1px solid red;
 
-    padding-left: 1.2rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    height: 4.4rem;
+
+    & > svg {
+      position: absolute;
+      top: 0;
+      left: 0;
+
+      margin-left: 1.2rem;
+    }
+  `,
+  ProgressBar: styled.div`
+    display: flex;
+    gap: 1.4rem;
   `,
 };
