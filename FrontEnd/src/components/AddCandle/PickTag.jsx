28f38452c4ee5cec3e79e54323671b9c72e67c3c
lydiacho <sheecho0804@ewhain.net>
2023-11-26 04:46:29 +0900

    style: PickTag 카드 ui 구현

--- FrontEnd/src/components/AddCandle/PickTag.jsx
+++ FrontEnd/src/components/AddCandle/PickTag.jsx
@@ -1,11 +1,39 @@
 import styled from 'styled-components';
 
 const PickTag = () => {
-  return <St.Wrapper>PickTag</St.Wrapper>;
+  const TAG = ['시원', 'SOPT', '승희'];
+  return (
+    <St.Wrapper>
+      <St.PickCard type='button'>시원</St.PickCard>
+      <St.GreyCard type='button'>SOPT</St.GreyCard>
+    </St.Wrapper>
+  );
 };
 
 export default PickTag;
 
 const St = {
   Wrapper: styled.section``,
+  PickCard: styled.button`
+    width: 8rem;
+    height: 8rem;
+
+    background-color: ${({ theme }) => theme.colors.red};
+    border: none;
+    border-radius: 1.2rem;
+
+    color: ${({ theme }) => theme.colors.white};
+    ${({ theme }) => theme.fonts.s3};
+  `,
+  GreyCard: styled.button`
+    width: 8rem;
+    height: 8rem;
+
+    background-color: ${({ theme }) => theme.colors.gray5};
+    border: none;
+    border-radius: 1.2rem;
+
+    color: ${({ theme }) => theme.colors.white};
+    ${({ theme }) => theme.fonts.s3};
+  `,
 };
