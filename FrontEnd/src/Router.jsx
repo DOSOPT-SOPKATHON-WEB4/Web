6e448ed04ea52cfb39637277bd92d292edb216b2
lydiacho <sheecho0804@ewhain.net>
2023-12-19 03:51:10 +0900

    feat: CandleDetailPage 생성

--- FrontEnd/src/Router.jsx
+++ FrontEnd/src/Router.jsx
@@ -4,6 +4,7 @@ import AddTagPage from './pages/AddTagPage';
 import AddCandlePage from './pages/AddCandlePage';
 import CakePage from './pages/CakePage';
 import OnboardingPage from './pages/OnboardingPage';
+import CandleDetailPage from './pages/CandleDetailPage';
 
 const Router = () => {
   return (
@@ -14,6 +15,7 @@ const Router = () => {
         <Route path='/add-tag' element={<AddTagPage />} />
         <Route path='/add-candle' element={<AddCandlePage />} />
         <Route path='/cake' element={<CakePage />} />
+        <Route path='/candle-detail' element={<CandleDetailPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ FrontEnd/src/pages/CandleDetailPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CandleDetailPage = () => {
+  return <div>CandleDetailPage</div>;
+};
+
+export default CandleDetailPage;
