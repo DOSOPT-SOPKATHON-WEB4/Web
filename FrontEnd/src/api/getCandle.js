00e4a6f12d47c3073042464e6fa5c62eccf566e0
seoAreum <1971236@hansung.ac.kr>
2023-12-21 03:18:50 +0900

    feat: 촛불 리스트를 가져오는 함수 생성

---
+++ FrontEnd/src/api/getCandle.js
@@ -0,0 +1,8 @@
+import api from './api';
+
+const getCandle = async (cakeId, setCandle) => {
+  const response = await api.get(`/cake/${cakeId}`);
+setCandle(response.data.data.candle_list)
+};
+
+export default getCandle;
