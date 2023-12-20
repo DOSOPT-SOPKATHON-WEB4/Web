2e5b1c335149cce9163541d9f49fd4b4afeac96a
lydiacho <sheecho0804@ewhain.net>
2023-12-21 03:45:46 +0900

    style: CandleDetailPage뷰 상세 수정

--- FrontEnd/src/components/CandleDetail/Description.jsx
+++ FrontEnd/src/components/CandleDetail/Description.jsx
@@ -8,12 +8,18 @@ export default Description;
 
 const St = {
   Wrapper: styled.section`
+    text-align: center;
+
     width: 100%;
     height: 100%;
+
+    padding: 1.6rem 2rem;
 
     background-color: ${({ theme }) => theme.colors.gray6};
     border-radius: 2rem;
 
+    ${({ theme }) => theme.fonts.b1};
+    color: ${({ theme }) => theme.colors.gray4};
     ${({ theme }) => theme.shadow.shadow};
   `,
 };
