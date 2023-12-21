33e3e7a964944b31427e36942bc4bcb864544da8
lydiacho <sheecho0804@ewhain.net>
2023-12-22 06:23:32 +0900

    feat: CandleDetailPage 변경된 CTA 반영

--- FrontEnd/src/pages/CandleDetailPage.jsx
+++ FrontEnd/src/pages/CandleDetailPage.jsx
@@ -4,17 +4,23 @@ import styled from 'styled-components';
 import Text from '../components/CandleDetail/Text';
 import Description from '../components/CandleDetail/Description';
 import LastUpdate from '../components/CandleDetail/LastUpdate';
-import Buttons from '../components/CandleDetail/Buttons';
 import useGetCandle from '../api/getCandle';
-import { useSearchParams } from 'react-router-dom';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 
 const CandleDetailPage = () => {
+  const navigate = useNavigate();
   const [searchParams] = useSearchParams();
   const [data, setData] = useState();
   const candleId = 1; // 추후 수정
   const cakeName = searchParams.get('cakeName');
 
   useGetCandle(candleId, cakeName, setData);
+
+  const resetCandle = () => {
+    navigate(-1);
+    // 다시 돌아갔을 땐 촛불이 reset되어야함. 해당 부분 아름이와 논의
+    // 돌아갈 때마다 촛불 위치가 변하는 이슈
+  };
 
   return (
     data && (
@@ -25,7 +31,9 @@ const CandleDetailPage = () => {
           <Description body={data.body} />
           <LastUpdate last={data.last_fire} />
         </div>
-        <Buttons />
+        <St.Btn type='button' onClick={resetCandle}>
+          촛불에 다시 불 붙이기
+        </St.Btn>
       </St.Wrapper>
     )
   );
@@ -55,5 +63,17 @@ const St = {
 
       gap: 2.2rem;
     }
+  `,
+  Btn: styled.button`
+    padding: 1.6rem 0rem;
+    width: 100%;
+
+    ${({ theme }) => theme.fonts.s3};
+    color: ${({ theme }) => theme.colors.white};
+
+    border: none;
+    border-radius: 10rem;
+    ${({ theme }) => theme.shadow.shadow};
+    background-color: ${({ theme }) => theme.colors.red};
   `,
 };
