fa121ffecc97b68433c9cbe3adecf02fc60275da
seoAreum <1971236@hansung.ac.kr>
2023-12-21 13:54:17 +0900

    chore: 코드 포맷팅

--- FrontEnd/src/api/getCandle.js
+++ FrontEnd/src/api/getCandle.js
@@ -2,7 +2,7 @@ import api from './api';
 
 const getCandle = async (cakeId, setCandle) => {
   const response = await api.get(`/cake/${cakeId}`);
-setCandle(response.data.data.candle_list)
+  setCandle(response.data.data.candle_list);
 };
 
 export default getCandle;
