86b955f4d1dfe1700aa7ff3927b5d5901afa1b24
lydiacho <sheecho0804@ewhain.net>
2023-12-22 22:32:38 +0900

    style: PickTag 카드 배치 수정

--- FrontEnd/src/components/AddCandle/PickTag.jsx
+++ FrontEnd/src/components/AddCandle/PickTag.jsx
@@ -10,18 +10,30 @@ const PickTag = ({ cakeTitle, setCakeTitle }) => {
   return (
     <St.Wrapper>
       {tag
-        .filter((el) => el.candle_count <= 8)
+        .filter((el) => el.candle_count === 0)
         .map((el, idx) => (
-          <St.Card
-            type='button'
-            key={idx}
-            onClick={() => {
-              setCakeTitle(el.cake_name);
-            }}
-            $isSelected={cakeTitle === el.cake_name}
-          >
-            {el.cake_name}
-          </St.Card>
+          <>
+            <St.Card
+              type='button'
+              key={idx}
+              onClick={() => {
+                setCakeTitle(el.cake_name);
+              }}
+              $isSelected={cakeTitle === el.cake_name}
+            >
+              {el.cake_name}
+            </St.Card>
+            <St.Card
+              type='button'
+              key={idx}
+              onClick={() => {
+                setCakeTitle(el.cake_name);
+              }}
+              $isSelected={cakeTitle === el.cake_name}
+            >
+              {el.cake_name}
+            </St.Card>
+          </>
         ))}
     </St.Wrapper>
   );
@@ -31,10 +43,13 @@ export default PickTag;
 
 const St = {
   Wrapper: styled.section`
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr;
+    display: flex;
+    justify-content: center;
     align-self: center;
     gap: 1.2rem;
+    flex-wrap: wrap;
+
+    padding: 0 2rem;
   `,
   Card: styled.button`
     width: 8rem;
