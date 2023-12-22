bed4587540f727db0519282676d5c3b3e03a9d62
seoAreum <1971236@hansung.ac.kr>
2023-12-22 18:40:35 +0900

    feat: api 관련 파일 정리

--- FrontEnd/src/api/getCake.js
+++ FrontEnd/src/api/getCake.js
@@ -1,8 +1,6 @@
-// import axios from 'axios';
 import api from './api';
 
 const getCake = async (setTag) => {
-  // const response = await axios.get(`${import.meta.env.VITE_BASE_URL}/cake`);
   const response = await api.get(`/cake`);
   setTag(response.data.data.cake_list);
 };
