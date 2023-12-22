956c55c7ed90a618a01bd2b227ed8ee4decd2153
seoAreum <1971236@hansung.ac.kr>
2023-12-22 20:05:33 +0900

    fix: post 주소 잘못 들어간 부분 수정

--- FrontEnd/src/api/postCandle.js
+++ FrontEnd/src/api/postCandle.js
@@ -2,7 +2,7 @@ import api from './api';
 
 const postCandle = async (info) => {
   try {
-    const response = await api.post(`candle`, info);
+    const response = await api.post(`/candle`, info);
     console.log(response);
   } catch (err) {
     console.log(err);
