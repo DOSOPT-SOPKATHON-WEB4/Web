bd4950a0ce146c727a3faa9b21051867844e0218
seoAreum <1971236@hansung.ac.kr>
2023-12-28 16:58:10 +0900

    fix: 캔들 패치 함수 이름 변경, navigate 관련 로직 주석(에러)

--- FrontEnd/src/api/patchCandle.js
+++ FrontEnd/src/api/patchCandle.js
@@ -1,15 +1,17 @@
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 import api from './api';
 
-const useGetCandle = async (candleId) => {
-  const navigate = useNavigate();
+const patchCandle = async (candleId) => {
+  // 이 함수 안에 navigate가 있는 경우, 유효하지 않은 훅 호출이라는 에러 발생 -> 여기서 경로 수정하면 안될 듯
+  // const navigate = useNavigate();
 
   try {
-    await api.patch(`/candle?candle=${candleId}`);
-    navigate(-1);
+    const response = await api.patch(`/candle?candle=${candleId}`);
+    console.log(response);
+    // navigate(-1);
   } catch (err) {
     console.log(err);
   }
 };
 
-export default useGetCandle;
+export default patchCandle;
